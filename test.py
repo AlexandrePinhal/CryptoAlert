8f6d6a0ef18f392b9be7f83b54fe3b318e0eaacc
alexandre pinhal <alexandre.pinhal@outlook.com>
2021-07-16 00:02:48 +0200

    Function that get the price from the website

--- test.py
+++ test.py
@@ -12,4 +12,7 @@ page = urllib.request.urlopen(urlpage) #return the thml to the variable page
 soup = BeautifulSoup(page, 'html.parser') #use beautifulsoup to analyse html
 
 price = (soup.find("div", {"class": "price-large"}).getText())
+price = price[1:]
 print(price)
+finalprice= price.replace(",", "")
+print(int(float(finalprice)))
