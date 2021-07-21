4410284505176444891c2ed86a991b370c9f4934
Alexandre Pinhal <AlexandrePinhal@outlook.com>
2021-07-21 16:36:49 +0200

    Project Crypto Alert sending you a Telegram message if the price of Bitcoin is outside of the defined limits

--- test.py
+++ test.py
@@ -3,16 +3,37 @@
 # import libraries
 from bs4 import BeautifulSoup
 import urllib.request
-import re
+import telegram
+from time import *
+
+
+                             ### GETTING THE PRICE ###
 
 # url of the source
 urlpage = 'https://www.coindesk.com/en/price/bitcoin'
 
-page = urllib.request.urlopen(urlpage) #return the thml to the variable page
+page = urllib.request.urlopen(urlpage) #return the html to the variable page
 soup = BeautifulSoup(page, 'html.parser') #use beautifulsoup to analyse html
 
 price = (soup.find("div", {"class": "price-large"}).getText())
 price = price[1:]
-print(price)
 finalprice= price.replace(",", "")
 print(int(float(finalprice)))
+
+                            ### SENDING A TELEGRAM CHAT ###
+
+def Telechat():
+    if (finalprice) < "30000" and (finalprice) > "25000" :
+        pass
+    else:
+        my_token = '1846958060:AAH0vB6Su8bhRVDXM78BVqhYYjIWyp-gQzc'
+
+        def send(msg, chat_id, token=my_token):
+            bot = telegram.Bot(token=token)
+            bot.sendMessage(chat_id=chat_id, text=msg)
+
+        send("Hey! The price of Bitcoin is {}€ now that's out of our limits".format(finalprice), "-1001566083560")
+
+while True:
+    sleep(2)
+    Telechat()
